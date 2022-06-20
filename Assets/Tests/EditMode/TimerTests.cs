8cd39eadc043da976471c744f29476356871d7ef
Lachezar Tsvetkov <lachezardts@gmail.com>
2022-06-20 18:35:59 +0300

    Implemented Slider for the timer

---
+++ Assets/Tests/EditMode/TimerTests.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class TimerTests
+{
+    [Test]
+    public void Paused()
+    {
+
+    }
+
+    [Test]
+    public void Active()
+    {
+
+    }
+
+    [Test]
+    public void Stopped()
+    {
+
+    }
+
+    [Test]
+    public void IncreasePaused()
+    {
+
+    }
+
+    [Test]
+    public void IncreaseActive()
+    {
+
+    }
+
+    [Test]
+    public void IncreaseStopped()
+    {
+
+    }
+
+    [Test]
+    public void StopActive()
+    {
+
+    }
+
+    [Test]
+    public void StopPaused()
+    {
+
+    }
+
+
+}
